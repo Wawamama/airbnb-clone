a7822a332f131a29e2db9c8dd54d51b360c49c02
Marion Saul <marionsaul.dev@gmail.com>
2022-07-25 10:20:33 +0200

    hide env var

--- utils/mongodb.ts
+++ utils/mongodb.ts
@@ -1,9 +1,7 @@
 import { MongoClient, ObjectId } from 'mongodb'
 
 export const connectToDb = async () => {
-	// const DATABASE_URL: string = process.env.DATABASE_URL as string
-	const DATABASE_URL: string =
-		'mongodb+srv://wawamama:test1234@testcluster0.zi9ufub.mongodb.net/sample_airbnb?retryWrites=true&w=majority'
+	const DATABASE_URL: string = process.env.DATABASE_URL as string
 	const client = await MongoClient.connect(DATABASE_URL)
 	if (!client) throw new Error('DB connection failed')
 	return client
